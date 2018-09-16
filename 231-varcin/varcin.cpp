9d403e3d9438a827cf7d033bceb467fc2085469c
s0ahc <zhengrong.han@outlook.com>
2018-09-16 12:25:56 +0800

    submit

--- 231-varcin/varcin.cpp
+++ 231-varcin/varcin.cpp
@@ -10,7 +10,10 @@ int main()
     cout<<c2<<endl;
     cout<<c<<endl;
     cout<<d<<endl;
-    getchar();
+    int dsd (2.4423213);
+    double sa,bgea=33123.23;
+    cout<<dsd<<endl;
+    getchar;
     
 }
 
