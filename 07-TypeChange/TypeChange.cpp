99d085cbd19a011769d0e4aae1d0d91fe22467a1
s0ahc <zhengrong.han@outlook.com>
2018-09-16 09:58:28 +0800

    varcin

--- 07-TypeChange/TypeChange.cpp
+++ 07-TypeChange/TypeChange.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 int main()
 {
-    int a;
-    a=-42;
+    int a=9;
+    int b=9;
+    //(a==b)?'true','false';
+    //a=-42;
     //bool bl;
+    /* 
     unsigned ui=2;
-    cout<<a+a<<endl;
+    cout<<a+a <<endl;
+    cout<<"\t tab \7 \n";
     cout<<ui+a<<endl;
+    */
     getchar();
 }
---
+++ 231-varcin/varcin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c1,c2;
+    int c;
+    float d;
+    cin>>c1>>c2>>c>>d;
+    cout<<c1<<endl;
+    cout<<c2<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    getchar();
+    
+}
+
