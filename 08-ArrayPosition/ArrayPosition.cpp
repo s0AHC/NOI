3b599bebcc5d88adc71aececcba875cf6f9256dc
s0AHC <zhengrong.han@outlook.com>
2018-11-26 11:36:03 +0800

    update C++ and C setting of VS code

---
+++ 08-ArrayPosition/ArrayPosition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int outside[9];
+int main()
+{
+        int i=0;
+        int inside[9];
+        cout<<" outside"<<" "<<" inside"<<endl;
+        for(0;i<=9;i++)
+        {
+                cout<<" "<<outside[i]<<"       ";
+                cout<<inside[i]<<endl;
+        }
+}
